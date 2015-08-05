e6f55573bbbe46ac81347961c8dc2ca96d02b855
efermin <efermin@contactop2p.com>
2015-08-05 14:56:17 -0430

    Se implementa logs con log4J

--- picService.sh
+++ picService.sh
@@ -6,12 +6,11 @@ case $1 in
 
 "start" )
 	echo "starting Pic Services"
-	sudo screen -dmS PIC /var/lib/jvm/jdk1.6.0_45/bin/java -jar asterisk-java-1.0.0.M3.jar		 
-	
+	/var/lib/jvm/jdk1.6.0_45/bin/java -cp lib/log4j-1.2.17.jar:lib/asterisk-java-1.0.0.M3.jar:lib/apache-log4j-extras-1.2.17.jar:lib/axis.jar:lib/axis-ant.jar:lib/axis-jaxrpc-1.3-sources.jar:lib/commons-discovery-0.2.jar:lib/commons-logging-1.0.4.jar:lib/javax.wsdl_1.6.2.v201012040545.jar:lib/jaxb-api-2.2.jar:lib/jaxrpc.jar:lib/jaxws-api.jar:lib/mail.jar:lib/org.apache.commons.logging-1.1.1.v201101211721.jar:lib/saaj.jar:lib/wsdl4j-1.5.1.jar:fastagi-mapping.properties:. org.asteriskjava.fastagi.DefaultAgiServer > /tcantv/logs/pic/pic-zte.log 2>&1 &
 ;;
 "stop" )
 	echo "Stoping Pic Services"
-	for i in `sudo screen -list | grep PIC| awk '{print $1}'`; do sudo screen -X -S $i quit; done
+	for i in `ps aux | grep DefaultAgiServer | grep -v grep | awk '{print $2}'`; do kill -9  $i; done
 ;;
 
 esac
